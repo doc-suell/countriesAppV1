a11dec3534cf05e8647a3a497d2930e833bc2c44
doc-suell <souhail888@hotmail.com>
2022-10-26 21:45:34 +0200

    adding countries

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Card from "./Card";
 
 const Countries = () => {
   // le useEffect se joue lorsque le composant est monté
   const [data, setData] = useState([]);
+  const [RangeValue, setRangeValue] = useState(36);
+  const radio = ["Afrique", "America", "Asia", "Europ", "Oceania"];
+  // const [hello, setHello] = useState("hello friends");
   useEffect(() => {
     // .get("link") aller chercher then. apres tu me donne un resultat "res" et tu l'affiche dans le console.log
 
@@ -13,7 +17,24 @@ const Countries = () => {
   }, []);
   return (
     <div className="countries">
-      <h1>COUNTRIES</h1>
+      <ul className="radio-container">
+        <input
+          type="range"
+          min="1"
+          max="250"
+          defaultValue={RangeValue}
+          onChange={(e) => setRangeValue(e.target.value)}
+        />
+        {radio.map(continent) => }
+        <input type="radio" id="afrique" />
+        <label htmlFor="afrique"></label>
+      </ul>
+      {/* <p>{hello}</p> */}
+      <ul>
+        {data.slice(0, RangeValue).map((country, index) => (
+          <Card key={index} country={country} />
+        ))}
+      </ul>
     </div>
   );
 };
